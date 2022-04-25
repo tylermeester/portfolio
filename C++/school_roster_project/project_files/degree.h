79b5c7842f6c3b86a39e38ba09b75063c2c0abc3
Tyler Meester <104333799+tylermeester@users.noreply.github.com>
2022-04-25 07:49:24 -0700

    Delete C++/school_roster_project directory

--- C++/school_roster_project/project_files/degree.h
+++
@@ -1,9 +0,0 @@
-/*====================================================================
-        C. DegreeProgram Enumerated Data Type Definition
-=====================================================================*/
-enum class DegreeProgram {
-    SECURITY = 0,
-    NETWORK = 1,
-    SOFTWARE = 2
-};
-
